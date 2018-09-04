28d1dbe41167c54504ee33ee88d621cc0b11a78b
yuishimimi <42453458+yuishimimi@users.noreply.github.com>
2018-09-04 12:30:46 -1000

    how to pull out if using brackets

---
+++ jrburger.js
@@ -0,0 +1,10 @@
+var jrburger = {
+ meat: "beef",
+ veg: ["lettuce","tomato", "pickles"],
+ cheese: "mozzarella",
+ mayo: true,
+ pepper: true,
+ price: 7,
+ quantity: 1
+}
+window.alert(jrburger.veg[1]);
